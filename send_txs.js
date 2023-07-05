75736f5b92ffbf54f8f90a5f84449fae1459d634
Jared Wasinger <j-wasinger@hotmail.com>
2023-07-05 16:40:59 +0200

    js works

--- send_txs.js
+++ send_txs.js
@@ -1,71 +1,60 @@
 const Web3 = require('web3')
-const web3 = new Web3('http://localhost:52540')
 const prefunded_pk = "..."
 const prefunded_account = "..."
+const { execSync } = require("node:child_process");
 
 let txCount = 1
 
-debugger
-
-signedTxs = []
-signedTx = web3.eth.signTransaction({
-		from:"878705ba3f8Bc32FCf7F4CAa1A35E72AF65CF766",
-		to:"34a600a929c439fcc9fd87bf493fea453add3d5a",
-		gas:"21000",
-		gasPrice: "1",
-		maxPriorityFeePerGas: "1",
-		maxFeePerGas: "1",
-		nonce: 0,
-		value:  "1000000000000000000",
-		data: "0xdf"
-	},
-	"ef5177cd0b6b21c87db5a0bf35d4084a8a57a9d6a064f86d51ac85f2b873a4e2",
-	function(err, transactionHash) {
-		if (!err)
-			console.log(transactionHash + " success");
-		console.log(err)
-	});
-
-/*
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function getPort() {
+	let output = execSync("python main.py").toString('utf-8')
+	debugger
+    	return parseInt(output.trim(), 10)
 }
 
-async function sendTxs(tx, count) {
-        let promises = []
-        for (let i = 0; i < count; i++) {
-                promises.push(new Promise((resolve, reject) => {web3.eth.sendTransaction(tx).then((x,y) => resolve())}))
-        }
-        return Promise.all(promises)
+async function signTx() {
+	return new Promise((resolve, reject) => {
+		web3.eth.signTransaction({
+				from:"878705ba3f8Bc32FCf7F4CAa1A35E72AF65CF766",
+				to:"34a600a929c439fcc9fd87bf493fea453add3d5a",
+				gas:"21016",
+				gasPrice: "1",
+				maxPriorityFeePerGas: "1",
+				maxFeePerGas: "1",
+				nonce: 0,
+				value:  "1000000000000000000",
+				data: "0xdf"
+			},
+			"ef5177cd0b6b21c87db5a0bf35d4084a8a57a9d6a064f86d51ac85f2b873a4e2",
+			function(err, transactionHash) {
+				if (!err)
+					resolve(transactionHash)
+				reject(err)
+			})
+	})
 }
 
-async function pollPending() {
-        for (;;) {
-                let pending = await new Promise((resolve, reject) => {
-                        web3.eth.getPendingTransactions().then((f) => resolve(f))})
-
-                if (pending.length == 0) {
-                        console.log("no more pending")
-                        return
-                } else {
-                        console.log("pending: ", pending.length)
-                }
-                await sleep(1000)
-        }
+async function sendTx(signedTx) {
+	return new Promise((resolve, reject) => {
+		web3.eth.sendSignedTransaction(signedTx.raw).then((thing) => {
+			debugger
+			resolve("foobar")
+		}, (error) => {
+			debugger
+		})
+	})
 }
+
+let web3 = null
 
 async function main() {
-        for (;;) {
-                console.log("sending more txs...")
-                await sendTxs({
-                        from:"34a600a929c439fcc9fd87bf493fea453add3d5f",
-                        to:"34a600a929c439fcc9fd87bf493fea453add3d50",
-                        value:  "1"//,
-                        //data: "0x60026000f3"
-                }, 4096)
-                await pollPending()
-        }
+	let port = getPort()
+	web3 = new Web3(`http://localhost:${port}`)
+	let signedTx = await signTx()
+	sendTx(signedTx)
 }
 
-main().then(() => {console.log("done")})
-*/
+main().then((done) => {
+	debugger
+}, (err) => {
+	debugger
+})
