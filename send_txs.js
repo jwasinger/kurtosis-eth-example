a65d9f1f890e161b46457293ebe4d039e7fa820d
Jared Wasinger <j-wasinger@hotmail.com>
2023-07-05 17:00:55 +0200

    more working

--- send_txs.js
+++ send_txs.js
@@ -4,15 +4,26 @@ const prefunded_account = "..."
 const { execSync } = require("node:child_process");
 
 let txCount = 1
+let nonce = 0
 
 function getPort() {
-	let output = execSync("python main.py").toString('utf-8')
-	debugger
+	let output = execSync("python3 main.py").toString('utf-8')
     	return parseInt(output.trim(), 10)
 }
 
 async function signTx() {
-	return new Promise((resolve, reject) => {
+	const getTxCount = new Promise((resolve, reject) => {
+		web3.eth.getTransactionCount("878705ba3f8Bc32FCf7F4CAa1A35E72AF65CF766", function(err, count) {
+			if (!err) {
+				resolve(count)
+			}
+			reject(err)
+		})
+	})
+
+	const txCount = await getTxCount
+
+	const signTx = new Promise((resolve, reject) => {
 		web3.eth.signTransaction({
 				from:"878705ba3f8Bc32FCf7F4CAa1A35E72AF65CF766",
 				to:"34a600a929c439fcc9fd87bf493fea453add3d5a",
@@ -20,26 +31,29 @@ async function signTx() {
 				gasPrice: "1",
 				maxPriorityFeePerGas: "1",
 				maxFeePerGas: "1",
-				nonce: 0,
+				nonce: txCount,
 				value:  "1000000000000000000",
 				data: "0xdf"
 			},
 			"ef5177cd0b6b21c87db5a0bf35d4084a8a57a9d6a064f86d51ac85f2b873a4e2",
-			function(err, transactionHash) {
-				if (!err)
-					resolve(transactionHash)
+			function(err, tx) {
+				if (!err) {
+					nonce += 1
+					resolve(tx.raw)
+				}
 				reject(err)
 			})
 	})
+
+	return await signTx
 }
 
 async function sendTx(signedTx) {
 	return new Promise((resolve, reject) => {
-		web3.eth.sendSignedTransaction(signedTx.raw).then((thing) => {
-			debugger
-			resolve("foobar")
+		web3.eth.sendSignedTransaction(signedTx).then((thing) => {
+			resolve(thing)
 		}, (error) => {
-			debugger
+			reject(error)
 		})
 	})
 }
@@ -50,11 +64,11 @@ async function main() {
 	let port = getPort()
 	web3 = new Web3(`http://localhost:${port}`)
 	let signedTx = await signTx()
-	sendTx(signedTx)
+	await sendTx(signedTx)
 }
 
 main().then((done) => {
 	debugger
 }, (err) => {
-	debugger
+	console.log("error: " + err)
 })
